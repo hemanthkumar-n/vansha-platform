4b749836411829a435c305428608c96fe74e6ab9
Hemanth Kumar <hemanthpresence@gmail.com>
2026-06-01 00:23:02 +0530

    refactor(graph): move founder family data into model

---
+++ mobile_app/lib/features/family/models/founder_family_data.dart
@@ -0,0 +1,43 @@
+class FounderFamilyData {
+  static const father = {
+    'name': 'Natakam Malakonda Prasad',
+    'relationship': 'Father',
+    'knownAs': 'Nanna',
+  };
+
+  static const mother = {
+    'name': 'Sudharani',
+    'relationship': 'Mother',
+    'knownAs': 'Amma',
+  };
+
+  static const self = {
+    'name': 'Hemanth Kumar',
+    'relationship': 'YOU',
+    'knownAs': 'Hemanth',
+  };
+
+  static const spouse = {
+    'name': 'Keerthi Doguparti',
+    'relationship': 'Spouse',
+    'knownAs': 'Keerthi',
+  };
+
+  static const child = {
+    'name': 'Yuvan Simha',
+    'relationship': 'Son',
+    'knownAs': 'Yuvan',
+  };
+
+  static const sister = {
+    'name': 'Divya Bharathi',
+    'relationship': 'Younger Sister',
+    'knownAs': 'Chelli',
+  };
+
+  static const brotherInLaw = {
+    'name': 'Buduri Kamesh',
+    'relationship': 'Brother In Law',
+    'knownAs': 'Kamesh Bava',
+  };
+}
